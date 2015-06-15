97c42c87baefc2391b4de5388705f45e83e47b1e
Nerielle <nelya.iva@gmail.com>
2015-06-15 17:45:10 +0300

    PrepareSetup added

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -1,2 +1,3 @@
 msbuild Scripts\root.msbuild /p:Configuration=Release /p:BUILD_NUMBER=1.2.3.4
 msbuild Scripts\UnitTests.msbuild /p:Configuration=Release
+msbuild Scripts\PrepareSetup.msbuild /p:Configuration=Release
