a035543badcf68b70063dfd59cab6027fa8c96ef
SergerGood <serzher@inbox.ru>
2015-06-16 09:24:56 +0300

    BuildSetup added

--- Scripts/build.bat
+++
@@ -1,3 +0,0 @@
-msbuild Scripts\root.msbuild /p:Configuration=Release /p:BUILD_NUMBER=1.2.3.4
-msbuild Scripts\UnitTests.msbuild /p:Configuration=Release
-msbuild Scripts\PrepareSetup.msbuild /p:Configuration=Release
---
+++ build.bat
@@ -0,0 +1,12 @@
+@echo off
+
+call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+
+pushd %~dp0
+
+msbuild root.msbuild /p:Configuration=Release /p:BUILD_NUMBER=1.2.3.4
+msbuild UnitTests.msbuild /p:Configuration=Release
+msbuild PrepareSetup.msbuild /p:Configuration=Release
+msbuild BuildSetup.msbuild /p:Configuration=Release
+
+pause
