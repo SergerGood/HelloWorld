808eb0a6f7502487b6020743365ee79f3b1c58c9
SergerGood <serzher@inbox.ru>
2015-06-16 10:18:38 +0300

    steps ordering

--- build.bat
+++ build.bat
@@ -6,9 +6,9 @@ pushd %~dp0
 
 msbuild root.msbuild /p:Configuration=Release /p:BUILD_NUMBER=1.2.3.4
 msbuild UnitTests.msbuild /p:Configuration=Release
+msbuild IntegrityCheckCreator.msbuild /p:Configuration=Release
 msbuild PrepareSetup.msbuild /p:Configuration=Release
 msbuild BuildSetup.msbuild /p:Configuration=Release
 msbuild Artifacts.msbuild /p:Configuration=Release
-msbuild IntegrityCheckCreator.msbuild /p:Configuration=Release
 
 pause
