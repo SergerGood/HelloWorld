306e68bc91d4573c567dd57ddfec1331f6ce2e1e
SergerGood <serzher@inbox.ru>
2015-06-16 09:32:26 +0300

    Artifacts added

--- build.bat
+++ build.bat
@@ -8,5 +8,6 @@ msbuild root.msbuild /p:Configuration=Release /p:BUILD_NUMBER=1.2.3.4
 msbuild UnitTests.msbuild /p:Configuration=Release
 msbuild PrepareSetup.msbuild /p:Configuration=Release
 msbuild BuildSetup.msbuild /p:Configuration=Release
+msbuild Artifacts.msbuild /p:Configuration=Release
 
 pause
